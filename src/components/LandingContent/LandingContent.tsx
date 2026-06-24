77904f80e3d7406d0583c1658ea38cdf701867e7
manivardhan2005 <manivardhanwoonna@gmail.com>
2026-06-24 23:13:40 +0530

    Completely rename Resume.pdf to lowercase in git

--- src/components/LandingContent/LandingContent.tsx
+++ src/components/LandingContent/LandingContent.tsx
@@ -85,7 +85,7 @@ export default function LandingContent() {
         </p>
         
         <a 
-          href="/Resume.pdf" 
+          href="/resume.pdf" 
           target="_blank" 
           rel="noopener noreferrer" 
           className={styles.resumeButton}
